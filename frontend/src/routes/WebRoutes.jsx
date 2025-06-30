9dbd86bb1c5a1b4e68ad8ed9fd1bf30d3f7765ec
HitarthRevakar <hitarthrevakar4@gmail.com>
2025-06-30 11:28:31 +0530

    fix webroutes

--- frontend/src/routes/WebRoutes.jsx
+++ frontend/src/routes/WebRoutes.jsx
@@ -1,32 +1,28 @@
-// ------------------------------- import data ------------------------------- //
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import WebLayoute from '../layoutes/WebLayoute'
-import HomePage from '../pages/website/HomePage'
-import Courses from '../pages/website/Courses'
-import Inquiry from '../pages/website/Inquiry'
-import AboutUs from '../pages/website/AboutUs'
-import ContactUs from '../pages/website/ContactUs'
-import CourseContent from '../pages/website/CourseContent'
-import NotFound from '../components/webComponents/NotFound'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import WebLayoute from '../layoutes/WebLayoute';
+import HomePage from '../pages/website/HomePage';
+import Courses from '../pages/website/Courses';
+import Inquiry from '../pages/website/Inquiry';
+import AboutUs from '../pages/website/AboutUs';
+import ContactUs from '../pages/website/ContactUs';
+import CourseContent from '../pages/website/CourseContent';
+import NotFound from '../components/webComponents/NotFound';
 
 const WebRoutes = () => {
   return (
     <Routes>
-        {/* ----------------------------- Admin Layout Wrapper ---------------------------- */}
-        <Route path='/' element={<WebLayoute/>}>
-
-        {/* ------------------------------ Default Route (Redirect to /dashboard) ------------------ */}
-            <Route index element={<HomePage />} />
-            <Route path='/courses' element={<Courses/>}/>
-            <Route path='/courses/coursecontent' element={<CourseContent/>}/>
-            <Route path='/inquiry' element={<Inquiry/>}/>
-            <Route path='/aboutus' element={<AboutUs/>}/>
-            <Route path='/contactus' element={<ContactUs/>}/>
-            <Route path="*" element={<NotFound />} />
-        </Route>
+      <Route path="/" element={<WebLayoute />}>
+        <Route index element={<HomePage />} />
+        <Route path="courses" element={<Courses />} />
+        <Route path="courses/coursecontent" element={<CourseContent />} />
+        <Route path="inquiry" element={<Inquiry />} />
+        <Route path="aboutus" element={<AboutUs />} />
+        <Route path="contactus" element={<ContactUs />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
-  )
-}
+  );
+};
 
 export default WebRoutes;
